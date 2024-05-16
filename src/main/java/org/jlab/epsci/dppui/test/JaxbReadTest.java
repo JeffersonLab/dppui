d6c7a36075c7f385fb9bee92e75b4ef865c8d3d8
gurjyan <gurjyan@jlab.org>
2024-05-16 14:33:20 -0400

    initial submit

---
+++ src/main/java/org/jlab/epsci/dppui/test/JaxbReadTest.java
@@ -0,0 +1,45 @@
+/*
+ *   Copyright (c) 2016.  Jefferson Lab (JLab). All rights reserved. Permission
+ *   to use, copy, modify, and distribute  this software and its documentation for
+ *   educational, research, and not-for-profit purposes, without fee and without a
+ *   signed licensing agreement.
+ *
+ *   IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL
+ *   INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
+ *   OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS
+ *   BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *   JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *   THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *   PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY,
+ *   PROVIDED HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE
+ *   MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ *
+ *   This software was developed under the United States Government license.
+ *   For more information contact author at gurjyan@jlab.org
+ *   Department of Experimental Nuclear Physics, Jefferson Lab.
+ */
+
+package org.jlab.epsci.dppui.test;
+
+import org.jlab.epsci.dppui.system.JCGComponent;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+import java.io.IOException;
+
+public class JaxbReadTest {
+    public static void main(String[] args) throws JAXBException, IOException {
+
+        String compName = args[0];
+
+        JAXBContext context = JAXBContext.newInstance(JCGComponent.class);
+
+        Unmarshaller um = context.createUnmarshaller();
+        JCGComponent comp = (JCGComponent) um.unmarshal(new File("/home/gurjyan/Test/"+compName+".xml"));
+        System.out.println(comp);
+    }
+
+}
